8b79ccd9b71d44d9101a406bb7fbdfcde03cfdcd
Robert Lowe <robertmlowe@rmlowe.com>
2020-06-21 00:49:34 +0800

    Promises

--- 4-callbacks.js
+++ 4-callbacks.js
@@ -1,33 +1,14 @@
-// setTimeout(() => {
-//   console.log('Two seconds are up');
-// }, 2000);
+const doWorkCallback = (callback) => {
+    setTimeout(() => {
+        // callback('This is my error!', undefined)
+        callback(undefined, [1, 4, 7])
+    }, 2000)
+}
 
-// const names = ['Andrew', 'Jen', 'Jess'];
-// const shortNames = names.filter(name => {
-//   return name.length <= 4;
-// });
+doWorkCallback((error, result) => {
+    if (error) {
+        return console.log(error)
+    }
 
-// const geocode = (address, callback) => {
-//   setTimeout(() => {
-//     const data = {
-//       latitude: 0,
-//       longitude: 0
-//     };
-
-//     callback(data);
-//   }, 2000);
-// };
-
-// geocode('Philadelphia', data => {
-//   console.log(data);
-// });
-
-const add = (a, b, callback) => {
-  setTimeout(() => {
-    callback(a + b);
-  }, 2000);
-};
-
-add(1, 4, sum => {
-  console.log(sum); // Should print: 5
-});
+    console.log(result)
+})
